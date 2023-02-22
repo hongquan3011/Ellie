9c98b52e1024299516a7ffc598009704726a32ee
ngochoangit2k1 <59520070+ngochoangit2k1@users.noreply.github.com>
2023-02-22 11:09:53 +0700

    update

--- js/product.js
+++ js/product.js
@@ -18,3 +18,30 @@ function slideImage(){
 
 window.addEventListener('resize', slideImage);
 
+const tabs = document.querySelectorAll(".tab-item");
+const panes = document.querySelectorAll(".tab-pane");
+const tabActive =   document.querySelector(".tab-item.active");
+
+tabs.forEach((tab, index) => {
+  const pane = panes[index];
+  tab.onclick = function () {
+    document.querySelector(".tab-item.active").classList.remove("active");
+    document.querySelector(".tab-pane.active").classList.remove("active");
+    this.classList.add("active");
+    pane.classList.add("active");
+  };
+  console.log(tab)
+});
+
+// count characters length
+function CountCharacters(character){
+    const characterLength = document.querySelector(".characters")
+     var char = 1500 - character.length;
+    characterLength.innerHTML = '('+char+ ')'
+    }
+    
+    const toggleComment = document.querySelector(".product-reviews__summary-comment")
+    const toggleProductReview = document.querySelector(".product-reviews__content-form")
+    toggleComment.onclick =() =>{
+      toggleProductReview.classList.toggle("open")
+    }
